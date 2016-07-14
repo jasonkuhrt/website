d1b6e341695905f2b6ba96a35f36c3c498792e03
Jason Kuhrt <jasonkuhrt@me.com>
2016-07-14 11:54:45 -0400

    Content: Add an "about" section.

--- journal.md
+++ journal.md
@@ -1,10 +1,19 @@
 # Journal
 
 
+## About Section
+
+* x Abort avatar goal because I do not think this addition is obviously better.
+* x Layout for sections after the banner.
+* x Add a section explaining my technical experience.
+* x Add a section with external profile links, email
+
+
+
 ## Setup a banner
 
-* ~~CSS loader~~
-* ~~Initial global styles for Type and colour~~
-* ~~Basic banner layout~~
-* File loader
-* Avatar asset
+* x CSS loader
+* x Initial global styles for Type and colour
+* x Basic banner layout
+* ~~File loader~~
+* ~~Avatar asset~~
--- src/components/app.js
+++ src/components/app.js
@@ -6,8 +6,21 @@ export default class App extends React.Component {
       <div id="app">
         <header id="banner">
           <h1 id="avatar">Jason Kuhrt</h1>
-          <p id="oneLiner">Someone shifting around these lines: Software Engineer – Interaction Designer – Systems Thinker – Graphic Artist<span className="DotMagenta">.</span> Devotee to the humanities<span className="DotCyan">.</span> In an alternate universe, xor: Coureur de Bois, Architect, Athlete, Lego Master Builder.</p>
+          <p>Someone shifting around these lines: Software Engineer – Interaction Designer – Systems Thinker – Graphic Artist<span className="DotMagenta">.</span> Devotee to the humanities<span className="DotCyan">.</span> In an alternate universe, xor: Coureur de Bois, Architect, Athlete, Lego Master Builder.</p>
         </header>
+        <section className="Section">
+          <h1>Work With Me</h1>
+          <p>Hi, I am a functional programmer with experience writing code for both frontend web applications, and backend web services. Recently I helped lead development of the realtime cloud platform at littleBits and before that architect the implementation for a large telcom website.</p>
+          <p>I graduated from design in a fine art faculty and incidentally can contribute an atypical perspective to traditional technology circles. Swapping my engineering hat out for research, conception, design, and so on is no problem.</p>
+          <p>I am currently based in Montreal and available for contract work. Feel free to get in touch, be it for a specific work/project opportunity or just a friendly hello.</p>
+        </section>
+        <ul>
+          <li><a href="mailto:jasonkuhrt@me.com">Email</a></li>
+          <li><a href="https://github.com/jasonkuhrt">Github</a></li>
+          <li><a href="https://www.behance.net/KUHRT">Behance</a></li>
+          <li><a href="https://twitter.com/JasonKuhrt">Twitter</a></li>
+          <li><a href="https://ca.linkedin.com/in/kuhrt">linkedIn</a></li>
+        </ul>
       </div>
     )
   }
