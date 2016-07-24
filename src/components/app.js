6f8a5072c14c53c9fb471ef7977e74ce002451d9
Jason Kuhrt <jasonkuhrt@me.com>
2016-07-24 11:21:30 -0400

    Update micro resume

--- src/components/app.js
+++ src/components/app.js
@@ -6,7 +6,7 @@ export default class App extends React.Component {
       <div id="app">
         <header id="banner">
           <h1 className="Avatar">Jason<br/>Kuhrt</h1>
-          <p>A romantic polymath shapeshifting amidst Art, Design, and Engineering<span className="DotMagenta">.</span> Devotee to the humanities<span className="DotCyan">.</span> In an alternate universe, xor: Coureur de Bois, Architect, Athlete, Lego Master Builder<span className="DotBlue">.</span></p>
+          <p>An aspiring polymath shapeshifting amidst Art, Design, and Engineering<span className="DotMagenta">.</span> Once described as "poet philosopher" amongst a "feral tribe of wandering alchemists after the apocolypse"<span className="DotPurple">.</span> Devotee to the humanities<span className="DotCyan">.</span> In an alternate universe, xor: Coureur de Bois, Architect, Athlete, Lego Master Builder<span className="DotBlue">.</span></p>
         </header>
         <section className="Section">
           <h1>Work With Me</h1>
