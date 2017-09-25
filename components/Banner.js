a4592da226084c6d134016dc77402fa336d96ccd
Jason Kuhrt <jason.kuhrt@ssense.com>
2017-09-25 12:45:01 -0400

    nextjs

---
+++ components/Banner.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+const Banner = () =>
+  <header id="banner">
+    <h1 className="Avatar">Jason<br/>Kuhrt</h1>
+    <p>An aspiring polymath shapeshifting amidst Art, Design, and Engineering<span className="DotMagenta">.</span> Once described as "poet philosopher" within a "feral tribe of wandering alchemists after the apocolypse"<span className="DotPurple">.</span> Devotee to the humanities<span className="DotCyan">.</span> In an alternate universe, xor: Coureur de Bois, Architect, Athlete, Lego Master Builder<span className="DotBlue">.</span></p>
+  </header>
+
+export default Banner
--- src/main.js
+++
@@ -1,8 +0,0 @@
-import "./main.css"
-import React from "react"
-import ReactDOM from "react-dom"
-import App from "./components/app"
-
-
-
-ReactDOM.render(<App />, document.getElementById("appContainer"))
