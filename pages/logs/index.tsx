4774e7fe1d3cb5552af4416e2f061aac8bb66f14
Jason Kuhrt <jasonkuhrt@me.com>
2022-09-04 20:56:44 -0400

    feat: use contentlayer (#4)

---
+++ pages/logs/index.tsx
@@ -0,0 +1,54 @@
+import Head from 'next/head'
+import Link from 'next/link'
+import { compareDesc, format, parseISO } from 'date-fns'
+import { GetStaticProps, NextPage } from 'next'
+import { FC } from 'react'
+import { allLogs, Log } from '../../.contentlayer/generated'
+
+export const getStaticProps: GetStaticProps = async () => {
+  const logs = allLogs.sort((a, b) => {
+    return compareDesc(new Date(a.date), new Date(b.date))
+  })
+  return { props: { logs } }
+}
+
+const LogCard: FC<Log> = (log) => {
+  return (
+    <div className="mb-6">
+      <time dateTime={log.date} className="block text-sm text-slate-600">
+        {format(parseISO(log.date), 'LLLL d, yyyy')}
+      </time>
+      <h2 className="text-lg">
+        <Link href={log.url}>
+          <a className="text-blue-700 hover:text-blue-900">{log.title}</a>
+        </Link>
+      </h2>
+    </div>
+  )
+}
+
+const Home: NextPage<{ logs: Log[] }> = ({ logs }) => {
+  return (
+    <div className="mx-auto max-w-2xl py-16 text-center">
+      <Head>
+        <title>Jason Kuhrt – Logs</title>
+      </Head>
+
+      <div className="mb-6 text-center">
+        <Link href="/">
+          <a title="home" className="text-center text-sm font-bold uppercase text-blue-700">
+            ↑
+          </a>
+        </Link>
+      </div>
+
+      <h1 className="mb-8 text-3xl font-bold">Logs</h1>
+
+      {logs.map((log, idx) => (
+        <LogCard key={idx} {...log} />
+      ))}
+    </div>
+  )
+}
+
+export default Home
