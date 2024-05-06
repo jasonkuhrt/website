b3a81f607038e8b66919ba75ef416d73e9c3bc12
Jason Kuhrt <jasonkuhrt@me.com>
2024-05-05 22:59:17 -0400

    title

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const siteTitle = 'Kuhrt'
+export const siteTitle = 'KUHRT'
 
 export const siteDescription = 'Personal website of Jason Kuhrt'
 
