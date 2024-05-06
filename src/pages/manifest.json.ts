1cee96f4edc19c7d9962dacef00a6b6767ab362d
Jason Kuhrt <jasonkuhrt@me.com>
2024-05-05 22:46:58 -0400

    fix manifest

--- src/pages/manifest.json.ts
+++ src/pages/manifest.json.ts
@@ -1,6 +1,6 @@
 import type { APIRoute } from 'astro'
 import { getImage } from 'astro:assets'
-import favicon from '../images/favicon.png'
+import favicon from '../../public/favicon.png'
 
 const faviconPngSizes = [370]
 
