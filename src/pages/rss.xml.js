489113cf891e8e7a393e957f28f71e06a3335cb2
Jason Kuhrt <jasonkuhrt@me.com>
2024-05-06 23:29:17 -0400

    fix: rss

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -3,14 +3,14 @@ import { getCollection } from 'astro:content'
 import { siteTitle, siteDescription } from '../consts'
 
 export async function GET(context) {
-  const posts = await getCollection('blog')
+  const posts = await getCollection('writing')
   return rss({
     title: siteTitle,
     description: siteDescription,
     site: context.site,
     items: posts.map((post) => ({
       ...post.data,
-      link: `/blog/${post.slug}/`,
+      link: `/writing/${post.slug}/`,
     })),
   })
 }
